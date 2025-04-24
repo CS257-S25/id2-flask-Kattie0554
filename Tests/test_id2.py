0ec35b4d1001b57622baed29424c98e645c74f7c
Kattie0554 <merazk@carleton.edu>
2025-04-24 13:19:56 -0500

    adding comms for syntax error

--- Tests/test_id2.py
+++ Tests/test_id2.py
@@ -6,12 +6,13 @@ class TestApp(unittest.TestCase):
         self.app=app.test_client()
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(b"This is the homepage:" \
-            "1) TO GET the top activity for a certain age, go to /get-top/'<'age'>'" \
-            ".... Note: for dummy data age options are: 23, 57, 80, 71, 40, 56, 18 " \
-            "2) TO GET a list of all category options, go to /get-all-categories "\
-            "....  NOTE: for now you can only shoose these categories: Personal Care Activities or Household Activities"\
-            " 3) TO GET a list of subcategory options from a category, go to /get-subcategories/'<'category'>'"\
-            "....  NOTE: for now lowkey just use Personal Care Activities as Category becuase the test data is incomplete"\
-            "4) TO GET a list of activities from a subcategory, go to /get-activities/'<'category'>'/'<'subcategory'>'"\
-            "....  NOTE: basically choose Personal Care Activities and Sleeping because of incomplete test data")
+            b"1) TO GET the top activity for a certain age, go to /get-top/'<'age'>'" \
+            b".... Note: for dummy data age options are: 23, 57, 80, 71, 40, 56, 18 " \
+            b"2) TO GET a list of all category options, go to /get-all-categories "\
+            b"....  NOTE: for now you can only shoose these categories: Personal Care Activities or Household Activities"\
+            b" 3) TO GET a list of subcategory options from a category, go to /get-subcategories/'<'category'>'"\
+            b"....  NOTE: for now lowkey just use Personal Care Activities as Category becuase the test data is incomplete"\
+            b"4) TO GET a list of activities from a subcategory, go to /get-activities/'<'category'>'/'<'subcategory'>'"\
+            b"....  NOTE: basically choose Personal Care Activities and Sleeping because of incomplete test data", response.data )
         
+    
