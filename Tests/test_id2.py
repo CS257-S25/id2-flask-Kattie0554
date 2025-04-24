63c4126d0aa2a636fe24b36ee701ab96d1a859ac
Kattie0554 <merazk@carleton.edu>
2025-04-24 14:32:07 -0500

    trying setUP

--- Tests/test_id2.py
+++ Tests/test_id2.py
@@ -1,11 +1,15 @@
 '''file: test_id2.py'''
 import unittest
-from app import *
+from app import app
 
 class TestApp(unittest.TestCase):
+    '''class for tests for app.py'''
+    def setUp(self):
+        '''set up for testing'''
+        app.config['TESTING']= True
+        self.app = app.test_client()
     def test_route_home(self):
         '''tests that the home route returns the correct thing'''
-        self.app = app.test_client()
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(b"This is the homepage:"\
             b"1) TO GET the top activity for a certain age, go to /get-top/'<'age'>'"\
@@ -23,13 +27,11 @@ class TestApp(unittest.TestCase):
             b"because of incomplete test data", response.data )
     def test_route_top_by_age(self):
         '''tests that the route to get top by age returns the right thing, given age 23'''
-        self.app = app.test_client()
         response = self.app.get('/get-top/23', follow_redirects=True)
         self.assertEqual(b"the top activity for people age 23 is T020901", response.data)
 
     def test_get_all_categories(self):
         '''tests that the route to get all categories returns the correct thing'''
-        self.app = app.test_client()
         response = self.app.get('/get-all-categories', follow_redirects=True)
         self.assertEqual(b"the categoy options are: "
             b"['Personal Care Activities', 'Household Activities', " \
@@ -45,8 +47,7 @@ class TestApp(unittest.TestCase):
 
     def test_get_subcategories_for_category(self):
         '''tets that the route to get subcatefories given a category returns the right thing '''
-        self.app = app.test_client()
-        response = self.app.get('/get-subcategories/Personal Care Activities', 
+        response = self.app.get('/get-subcategories/Personal Care Activities',
                                 follow_redirects=True)
         self.assertEqual(b"these are the subcategories: "
         b"['Sleeping', 'Grooming', 'Health-related self care', " \
@@ -55,10 +56,9 @@ class TestApp(unittest.TestCase):
 
     def test_get_activities_from_sub(self):
         '''tests that the route to get activities returns the correct thing '''
-        self.app = app.test_client()
-        response = self.app.get('/get-activities/Personal Care Activities/Sleeping', 
+        response = self.app.get('/get-activities/Personal Care Activities/Sleeping',
                                 follow_redirects=True)
         self.assertEqual(b"here are the activities for Sleeping in Personal Care Activities: "
         b"['Sleeping', 'Sleeplessness']", response.data)
-    #need to make tests for cases where they forget to add a thing like /category also if they spell something wrong
-
+    #need to make tests for cases where they forget to add a thing like /category 
+    # also if they spell something wrong
