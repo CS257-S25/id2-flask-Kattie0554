3f743a33b0cb2fbcfbc1033ef3dc648a3fbdad0a
Kattie0554 <merazk@carleton.edu>
2025-04-24 14:19:19 -0500

    trying to fix test style

--- Tests/test_id2.py
+++ Tests/test_id2.py
@@ -4,6 +4,7 @@ from app import *
 
 class TestApp(unittest.TestCase):
     def test_route_home(self):
+        '''tests that the home route returns the correct thing'''
         self.app = app.test_client()
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(b"This is the homepage:"\
@@ -21,11 +22,13 @@ class TestApp(unittest.TestCase):
             b"....  NOTE: basically choose Personal Care Activities and Sleeping "\
             b"because of incomplete test data", response.data )
     def test_route_top_by_age(self):
+        '''tests that the route to get top by age returns the right thing, given age 23'''
         self.app = app.test_client()
         response = self.app.get('/get-top/23', follow_redirects=True)
         self.assertEqual(b"the top activity for people age 23 is T020901", response.data)
 
     def test_get_all_categories(self):
+        '''tests that the route to get all categories returns the correct thing'''
         self.app = app.test_client()
         response = self.app.get('/get-all-categories', follow_redirects=True)
         self.assertEqual(b"the categoy options are: ['Personal Care Activities', 'Household Activities', " \
@@ -36,6 +39,7 @@ class TestApp(unittest.TestCase):
             b"'Volunteer Activities', 'Telephone Calls', 'Traveling']", response.data)
 
     def test_get_subcategories_for_category(self):
+        '''tets that the route to get subcatefories given a category returns the right thing '''
         self.app = app.test_client()
         response = self.app.get('/get-subcategories/Personal Care Activities', follow_redirects=True)
         self.assertEqual(b"these are the subcategories: "
@@ -43,6 +47,7 @@ class TestApp(unittest.TestCase):
         b"for Personal Care Activities", response.data)
 
     def test_get_activities_from_sub(self):
+        '''tests that the route to get activities returns the correct thing '''
         self.app = app.test_client()
         response = self.app.get('/get-activities/Personal Care Activities/Sleeping', follow_redirects=True)
         self.assertEqual(b"here are the activities for Sleeping in Personal Care Activities: ['Sleeping', 'Sleeplessness']", response.data)
